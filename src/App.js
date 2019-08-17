5a71e0b4f8fff6ee551f732846888c7af5d47f3d
Andrew <ajlutheran@gmail.com>
2019-08-17 17:06:45 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Helmet } from "react-helmet"
+import "./App.css";
+import SearchResult from "./SearchResult";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [],
+    }
+  }
+  onChange = (e) => {
+    const terms = e.target.value.split(" ");
+    const lastWord = terms[terms.length - 1];
+
+    fetch(`https://api.datamuse.com/words?rel_rhy=${lastWord}&md=d`).then(res => {
+      return res.json();
+    }).then(words => this.setState({ searchResults: words }))
+  }
+  render() {
+    console.log(this.state.searchResults)
+    return (
+      <>
+        <Helmet title="Find a Dope Rhyme">
+          <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        </Helmet>
+        <div className="container">
+          <div className="title">Find a Rhyme</div>
+          <input type="text" onChange={this.onChange} className="searchInput" placeholder="Start typing to find words that rhyme" />
+          <div className="resultsContainer">
+            {
+              this.state.searchResults.map(result => <SearchResult result={result} />)
+            }
+          </div>
+        </div>
+      </>
+    )
+  }
 }
-
-export default App;
